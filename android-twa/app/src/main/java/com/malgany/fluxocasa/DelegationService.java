b5d44306bbec4011deb9d856e5a9bf6ee04368ab
malgany <yopsadida@gmail.com>
2026-05-27 16:45:02 -0400

    Track remaining Android TWA sources

---
+++ android-twa/app/src/main/java/com/malgany/fluxocasa/DelegationService.java
@@ -0,0 +1,13 @@
+package com.malgany.fluxocasaapp;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
